d4efc308d539dc157522ec7d0cb3bbef40ddcf06
parkbinn <116538616+parkbinn@users.noreply.github.com>
2022-10-26 01:45:14 +0900

    1_function

---
+++ 1일차_문법/python_function.py
@@ -0,0 +1,42 @@
+# def name(input) : 
+#     print("제 이름은 " + input + "입니다")
+
+#     name("인섭")
+#     def sum(a,b):
+#         print("a+b")
+#         print(a+b) #7
+#         return a+b
+
+# total = sum(3,4)
+# print("total")
+# print(total) #none
+
+#성적과 이름을 입력하고, 성적에 따른 등급을 알려주는 프로그램을 만드시오.
+#단, 함수를 사용하시오.
+
+#90점 이상 A등급
+#80점 이상 B등급
+#70점 이상 C등급
+#60점 이상 D등급
+#60점 미만 F등급
+
+# grade = ""
+# def check_grade(성적, 이름):
+#     if 성적 >=90:
+#         grade = 'A'
+#     elif 성적 >=80:
+#         grade = 'B'
+#     return grade
+
+# a = check_grade(100)
+# print(a)
+
+#range (1,10)
+# len()
+# print()
+int()
+
+a = "python"
+a = a.upper()
+"Python", "python"
+print(a)
