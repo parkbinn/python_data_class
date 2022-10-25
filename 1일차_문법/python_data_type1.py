999cdd4aa5baa30c0453fd2c7d25d96e41382cbf
parkbinn <116538616+parkbinn@users.noreply.github.com>
2022-10-26 01:45:02 +0900

    1_data type

---
+++ 1일차_문법/python_data_type1.py
@@ -0,0 +1,42 @@
+from msilib import _directories
+
+
+x = "python" #문자형
+X = 123
+x= "123"
+X = [1, 2, 3, 4]
+x = {"name":"leo", "age":32}
+x = True # False
+
+
+a = 1 + 1
+a = 100 - 1
+a= 12 * 12
+a = 3/4
+# print(a)
+
+a = "Act as thouth \nit is impossible"
+
+a = ' "Failure is simply the opportunity to begin again." he says." '
+a = """ "Failure is simply the opportunity to begin agin """
+c = "hello python!"
+
+c[0] #indexing
+print(c[0:4]) #slicing, 0 <= c < 5
+print(c[6:13])
+print (c[:])
+
+x = "TitanicJames"
+title = print(x[:7]) #indexing
+director = print(x[7:])
+#print(title,director)
+
+
+a = ["a", "b", ["c", "d", "e"]]
+print(a[1])print(a[2])
+
+x = {"key1":"value1", "key2":"value2"}
+printx("key2")
+
+x = set(["5","2","1"])
+print(x)
